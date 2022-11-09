0ac87da320c0ee0cbaaab0e3643b7a26d6bf0d33
Ben Brooks <ben@bbrks.me>
2022-11-09 10:40:41 +0000

    Add DALL-E test fixture image (1024x uncompressed PNG)

--- fixtures_test.go
+++ fixtures_test.go
@@ -7,6 +7,7 @@ var testFixtures = []struct {
 }{
 	{"fixtures/test.png", "LFE.@D9F01_2%L%MIVD*9Goe-;WB", 4, 3},
 	{"fixtures/octocat.png", "LNAdApj[00aymkj[TKay9}ay-Sj[", 4, 3},
+	{"fixtures/dalle.png", "eaF#5R0#WBjYR+58-nWCWBn~bIsTbbayjFWof8jFj[WX-nNHR*jss.", 5, 5},
 	{"", "LNMF%n00%#MwS|WCWEM{R*bbWBbH", 4, 3},
 	{"", "KJG8_@Dgx]_4V?xuyE%NRj", 3, 3},
 }
